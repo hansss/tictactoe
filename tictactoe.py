89f6bdbab0e40ef6f4a3ac25dbfcad9e463be3d1
Alexander Solis <solis@college.harvard.edu>
2015-04-14 04:11:05 -0400

    End of Day 1

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,15 @@
 import sys
 
 #Randomize the first move to decide who plays#
+class HelperFunctions(object):  
+   def not_taken(self, board, move):
+     if board.positions[int(move)] != move:
+      return True
+     else: 
+      return False  
+      
+   #def valid_character
+     
 
 class Board (object):
    def __init__(self): 
@@ -34,13 +43,44 @@ class Board (object):
    
    def players_move(self):
     move = raw_input("Make your move: ")
-    print "You entered", move
-    self.positions[int(move)] = self.player
-    self.to_string()
     
-  
+    #check that the spot is not taken
+    if move.isdigit() and int(move) in self.positions.keys():
+     if HelperFunctions.not_taken(HelperFunctions(),self,move):
+      print "This position is already taken. Choose a different move."
+      self.players_move()
+     else:
+      print "You entered", move
+      self.positions[int(move)] = self.player
+      self.to_string()
+      self.players_move()
+    else:
+      print "This is not a valid move. Please choose again."
+      self.players_move()
     
+     
+    """
+    if HelperFunctions.not_taken(HelperFunctions(),self,move):
+     print "This position is already taken. Choose a different move."
+     self.players_move()
+    else:
+     return self.keys()
     
+     if move in self.keys():
+      print "You entered", move
+      self.positions[int(move)] = self.player
+      self.to_string()
+      self.players_move()
+     else:
+      print "This is not a valid move. Please choose again."
+      self.players_move()
+  """
+    
+    #check that the input is 1-9
+    #check if this is a winning move
+
+    
+
 board = Board()
 board.to_string()
 board.players_move()
